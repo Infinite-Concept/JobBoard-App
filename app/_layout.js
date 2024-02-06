c02cd78cea18dc5eda33c6fea175cf4acace6497
Infinite-Concept <ifenowoifesegun@gmail.com>
2024-02-06 16:13:05 +0100

    Job Board application using react native and rapid job api

---
+++ app/_layout.js
@@ -0,0 +1,34 @@
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    const onLayoutRootview = useCallback( async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+    
+    if (!fontsLoaded) {
+        return null
+    }
+    
+    return <Stack onLayout={onLayoutRootview} />
+    
+    // (
+    //     <Stack initialRouteName="home">
+    //       <Stack.Screen name="home" />
+    //     </Stack>
+    //   )
+}
+
+export default Layout;
